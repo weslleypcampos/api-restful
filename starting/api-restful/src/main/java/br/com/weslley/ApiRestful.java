555784ee9bb373d28ae40aeb7e4baa696714d0b9
unknown <weslley_1997campos@hotmail.com>
2023-06-21 14:00:49 -0300

    starting

---
+++ starting/api-restful/src/main/java/br/com/weslley/ApiRestful.java
@@ -0,0 +1,13 @@
+package br.com.weslley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestful {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestful.class, args);
+	}
+
+}
---
+++ starting/api-restful/src/test/java/br/com/weslley/ApiRestfulTests.java
@@ -0,0 +1,13 @@
+package br.com.weslley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
